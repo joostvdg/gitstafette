9c8f745ad3dca0fa6f1ed42e86f822f4f458ccd1
Joost van der Griendt <joostvdg@gmail.com>
2024-09-03 13:03:27 +0200

    add first tf bits

---
+++ aws/test-ec2-secrets/terraform/main.tf
@@ -0,0 +1,109 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.6.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.gistafette.id
+  vpc      = true
+}
+
+# datasource for vpc to collect object?
+data "aws_vpc" "vpc" {
+  id = var.vpc_id
+}
+
+data "aws_subnet" "selected" {
+  id = var.subnet_id
+}
+
+data "aws_internet_gateway" "default" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+data "aws_security_group" "aws-linux-sg" {
+  vpc_id = data.aws_vpc.vpc.id
+  filter {
+    name   = "group-name"
+    values = ["linux-sg"]
+  }
+}
+
+# Define the security group for the Linux server
+# resource "aws_security_group" "aws-linux-sg" {
+#   name        = "linux-sg"
+#   description = "Allow incoming traffic to the Linux EC2 Instance"
+#   vpc_id      = data.aws_vpc.vpc.id
+#
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["77.174.22.146/32"]
+#     description = "Allow incoming SSH connections"
+#   }
+#
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Web server"
+#   }
+#
+#   ingress {
+#     from_port   = 50051
+#     to_port     = 50051
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "GPRC Server"
+#   }
+#
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+# Create EC2 Instance
+resource "aws_instance" "gistafette" {
+  ami                         = var.ami_id
+  instance_type               = var.linux_instance_type
+  subnet_id                   = data.aws_subnet.selected.id
+  vpc_security_group_ids      = [data.aws_security_group.aws-linux-sg.id]
+  associate_public_ip_address = var.linux_associate_public_ip_address
+  source_dest_check           = false
+  key_name                    = var.ssh_key_name
+  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
+
+
+  # root disk
+  root_block_device {
+    volume_size           = var.linux_root_volume_size
+    volume_type           = var.linux_root_volume_type
+    delete_on_termination = true
+    encrypted             = true
+  }
+
+  user_data = file("${path.module}/startup.sh")
+
+  tags_all = {
+    Name = "gistafette-demo"
+  }
+}
---
+++ aws/test-ec2-secrets/terraform/vars.tf
@@ -0,0 +1,79 @@
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key"
+  sensitive   = true
+}
+
+variable "ami_id" {
+  description = "ID of the AMI"
+  default     = "ami-0197cd16f31a182d2"
+  // new AMI from 2024-07-04 ami-09564fc493f107560
+}
+
+variable "ec2_role_name" {
+  type        = string
+  description = "Name of the EC2 role"
+  default     = "ec2-read-gitstafette-secrets"
+}
+
+variable "ssh_key_name" {
+  description = "AWS SSH Key Name"
+  default     = "gitstafette"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-central-1"
+}
+
+# AWS AZ
+variable "aws_az" {
+  type        = string
+  description = "AWS AZ"
+  default     = "eu-central-1b"
+}
+
+# VPC Variables
+variable "vpc_id" {
+  type        = string
+  description = "ID of your default VPC"
+  default     = "vpc-a13a59ca"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "ID of the default subnet of your default VPC"
+  default     = "subnet-84b1d9c9"
+}
+
+variable "linux_instance_type" {
+  type        = string
+  description = "EC2 instance type for Linux Server"
+  default     = "t4g.nano"
+}
+
+variable "linux_associate_public_ip_address" {
+  type        = bool
+  description = "Associate a public IP address to the EC2 instance"
+  default     = true
+}
+
+variable "linux_root_volume_size" {
+  type        = number
+  description = "Size of root volume of Linux Server"
+  default     = 25
+}
+
+
+variable "linux_root_volume_type" {
+  type        = string
+  description = "Type of root volume of Linux Server."
+  default     = "gp2"
+}
